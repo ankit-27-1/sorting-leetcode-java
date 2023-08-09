868798650e38ee887b57ae5d5dd0032466cc1f34
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-08-09 19:20:47 +0530

    Create sortThePeople.java
    You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
    
    For each index i, names[i] and heights[i] denote the name and height of the ith person.
    
    Return names sorted in descending order by the people's heights.

---
+++ sortThePeople.java
@@ -0,0 +1,19 @@
+class sortThePeople {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n=heights.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(heights[i]<heights[j]){
+                    int temp=heights[i];
+                    heights[i]=heights[j];
+                    heights[j]=temp;
+
+                    String temp1=names[i];
+                    names[i]=names[j];
+                    names[j]=temp1;
+                }
+            }
+        }
+        return names;
+    }
+}
